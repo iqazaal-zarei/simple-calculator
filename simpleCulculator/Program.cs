13f1b39c9ab8cb89d43c45acb4df8b4f695d8274
iqazzal-zarei <gzarei202@gmail.com>
2023-07-24 11:57:56 +0330

    this is culculator

---
+++ simpleCulculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCulculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter num1 : ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Operator : ");
+            string oprator = Console.ReadLine();
+
+            Console.Write(" Enter num2 : ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (oprator == "+")
+            {
+                Console.WriteLine("Answer is :  " + num1 + num2);
+            }
+            else if (oprator == "-")
+            {
+                Console.WriteLine("Answer is : " + (num1 - num2));
+            }
+            else if (oprator == "*")
+            {
+                Console.WriteLine("Answer is : " + num1 * num2);
+            }
+
+            else if (oprator == "/")
+            {
+                Console.WriteLine("Answer is: " + num1 / num2);
+            }
+
+            else if (oprator == "%")
+            {
+                Console.WriteLine("Answer is : " + num1 % num2);
+            }
+            else
+            {
+                Console.WriteLine("invalid operator ");
+            }
+                Console.ReadLine();
+            
+        }
+    }
+}
